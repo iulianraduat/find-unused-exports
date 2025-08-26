557d0251dfaa36db1c326daa0a3d484cfdba8010
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2025-08-26 13:00:49 +0200

    test: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto',
+};
